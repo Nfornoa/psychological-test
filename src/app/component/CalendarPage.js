e4a2b84127e82402295b8b8efa7120aeb6041f92
Nfornoa <tyr0267@gmail.com>
2025-05-01 14:23:35 +0800

    updates

---
+++ src/app/component/CalendarPage.js
@@ -0,0 +1,23 @@
+'use client';
+
+
+import React from 'react';
+import { DatePicker } from 'antd';
+
+
+
+
+export default function CalendarPage() {
+
+  return (
+    <>
+     <div className='relative bg-gray-500 w-[320px] h-[480px] rounded-2xl flex items-center justify-center flex-col'>
+          <div className='absolute top-4 left-4'> 2025 </div>
+          <div className='absolute top-4 right-4'> 五月 </div>
+          <div className='text-[240px]'> 01 </div>
+          <div className='text-[60px]'> 星期四 </div>
+        </div>
+
+    </>   
+  );
+}
--- src/app/test-ui/page.js
+++ src/app/test-ui/page.js
@@ -17,15 +17,63 @@ export default function TestUI() {
   
 
   return (
-    <div className="max-w-2xl mx-auto p-6">
-      
-      <DatePicker onChange={onChange} needConfirm/>
-      <div>
-        <div>2025</div>
-        <div>五月</div>
-        <div>一號</div>
-        <div>星期四</div>
+    <div className="max-w-2xl mx-auto p-6 gap-4 flex-col overflow-x-auto">
+      {/* <DatePicker onChange={onChange} needConfirm/> */}
+
+
+        <div className='relative bg-gray-500 w-[320px] h-[480px] rounded-2xl flex items-center justify-center flex-col'>
+          <div className='absolute top-4 left-4'> 2025 </div>
+          <div className='absolute top-4 right-4'> 五月 </div>
+          <div className='text-[240px]'> 01 </div>
+          <div className='text-[60px]'> 星期四 </div>
+        </div>
+
+        <div className='relative bg-gray-500 w-[320px] h-[480px] rounded-2xl flex items-center justify-center flex-col'>
+          <div className='absolute top-4 left-4'> 2025 </div>
+          <div className='absolute top-4 right-4'> 五月 </div>
+          <div className='text-[240px]'> 02 </div>
+          <div className='text-[60px]'> 星期五 </div>
+        </div>
+
+
+        <div className='relative bg-gray-500 w-[320px] h-[480px] rounded-2xl flex items-center justify-center flex-col'>
+          <div className='absolute top-4 left-4'> 2025 </div>
+          <div className='absolute top-4 right-4'> 五月 </div>
+          <div className='text-[240px]'> 03 </div>
+          <div className='text-[60px]'> 星期六 </div>
+        </div>
+
+
+        <div className='relative bg-gray-500 w-[320px] h-[480px] rounded-2xl flex items-center justify-center flex-col'>
+          <div className='absolute top-4 left-4'> 2025 </div>
+          <div className='absolute top-4 right-4'> 五月 </div>
+          <div className='text-[240px]'> 04 </div>
+          <div className='text-[60px]'> 星期日 </div>
+        </div>
+
+        <div className='relative bg-gray-500 w-[320px] h-[480px] rounded-2xl flex items-center justify-center flex-col'>
+          <div className='absolute top-4 left-4'> 2025 </div>
+          <div className='absolute top-4 right-4'> 五月 </div>
+          <div className='text-[240px]'> 05 </div>
+          <div className='text-[60px]'> 星期一一 </div>
+        </div>
+
+        <div className='relative bg-gray-500 w-[320px] h-[480px] rounded-2xl flex items-center justify-center flex-col'>
+          <div className='absolute top-4 left-4'> 2025 </div>
+          <div className='absolute top-4 right-4'> 五月 </div>
+          <div className='text-[240px]'> 06 </div>
+          <div className='text-[60px]'> 星期二 </div>
+        </div>
+
+        <div className='relative bg-gray-500 w-[320px] h-[480px] rounded-2xl flex items-center justify-center flex-col'>
+          <div className='absolute top-4 left-4'> 2025 </div>
+          <div className='absolute top-4 right-4'> 五月 </div>
+          <div className='text-[240px]'> 07 </div>
+          <div className='text-[60px]'> 星期三 </div>
+        </div>
+
+        
       </div>
-      </div>
+     
   );
 }
